c7799cd326f9d45faae2c3036bfc49d07279a459
Ibrahimcode27 <ibrahimmulla2727@gmail.com>
2025-02-05 20:00:21 +0530

    file upload

---
+++ 5thfeb.js
@@ -0,0 +1,68 @@
+//1- Variable declaration 
+// var, let, const
+
+const name = "Ibrahim Mulla"
+const age = 22
+const PhoneNumber = 8850734982
+
+console.log(name);
+console.log(age);
+console.log(PhoneNumber);
+console.log(`My name is ${name}. I am ${age} Years Old and you can Call me on ${PhoneNumber} 😏`);
+
+var name1 = "Ibrahim"
+console.log(name1)
+
+let myname = "ebrohim"
+console.log(
+  `My name is ${myname}. I am ${age} Years Old and you can Call me on ${PhoneNumber} 😏`
+)
+// understood variables 🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳
+
+//Artithematic Operators(+,-,*,/,%)
+console.log(4 + 4);
+console.log(4 - 4);
+console.log(4 / 4);
+console.log(4%4)
+
+// understood maths Operators 😁😁
+
+
+//Logical Operators (>,<,>=,<=)
+
+console.log(4>4)
+console.log(4 < 4);
+console.log(4 >= 4);
+
+
+let number1 = 26
+let number2 = 445
+
+console.log(number1=number2)
+
+
+const againnumber = "45"
+const againnumber2= 45
+console.log(!againnumber===againnumber2)// this will give true because 2= don't compares data types. 
+console.log(!againnumber===againnumber2)// this will give false because 3= compares data types. 
+
+// AND = &&
+// OR = ||
+const thisisfalse = false
+const thisistrue = true
+
+console.log(thisisfalse&&thisistrue)// and will give false
+console.log(thisisfalse && thisistrue)// or will give true 
+
+// undefined and null are data types
+// undefined and null are treated as false 
+
+let loginid= "junedkhanabcde"
+let password = 2342532
+
+if(!loginid){
+    console.log("this is error")
+}
+
+console.log("null and undefined also taught well :)")
+console.log("THANK YOU EVERY MUCH!!!😭😭😭😭😭")
